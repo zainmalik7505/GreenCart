df7b8b9de279d0366eb598ffbf617e4a9a2950b8
Zain Malik <xaineil121@gmail.com>
2024-10-15 15:32:44 +0500

    Initial Commit

---
+++ GreenCart/GreenCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenCartApp.swift
+//  GreenCart
+//
+//  Created by Zain Malik on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GreenCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreenCartTests/GreenCartTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreenCartTests.swift
+//  GreenCartTests
+//
+//  Created by Zain Malik on 15/10/2024.
+//
+
+import Testing
+@testable import GreenCart
+
+struct GreenCartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
